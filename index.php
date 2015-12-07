7ca3cd8fe780e2a32cb56f61233a363a4087896d
Anderson <andersonmegax@hotmail.com>
2015-12-07 01:40:17 -0300

    Correção para inserir cadastro no banco de dados

--- index.php
+++ index.php
@@ -12,5 +12,12 @@ headertype();
 vericaStausInicial();
 footer();
 
+$CONECT = mysqli_connect(HOST, USER, PASS, DB);
+//  mysqli_select_db($CONECT);
+if(!$CONECT){
+    echo"Could not connect: ".mysqli_error();
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+}
 
-//echo "Index";
