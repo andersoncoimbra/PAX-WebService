3862547f0c4930f9676155e6dd0f1be0a9b6e0cf
Anderson <andersonmegax@hotmail.com>
2015-12-06 21:28:21 -0300

    correções de herança

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
  * Date: 03/12/2015
  * Time: 22:59
  */
-include "model/model.inc";
-include "view/view.inc";
+include_once "model/model.inc";
+include_once "view/view.inc";
 
 headertype();
 vericaStausInicial();
