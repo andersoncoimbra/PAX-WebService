33b5bdbeb9842a3fa59b676591afca025aa3226e
Anderson <andersonmegax@hotmail.com>
2015-12-09 05:17:11 -0300

    criação de uma area de administrador, criação de metosdos de consulta e correçoes de falhas

---
+++ admin/admin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Anderson
+ * Date: 09/12/2015
+ * Time: 01:25
+ */
+function admin(){
+    echo "Area de Administração";
+    allAlunos();
+}
+
--- index.php
+++ index.php
@@ -7,17 +7,10 @@
  */
 include_once "model/model.inc";
 include_once "view/view.inc";
+include_once 'admin/admin.php';
+include_once 'control/control.inc';
 
 headertype();
 vericaStausInicial();
 footer();
-
-$CONECT = mysqli_connect(HOST, USER, PASS, DB);
-//  mysqli_select_db($CONECT);
-if(!$CONECT){
-    echo"Could not connect: ".mysqli_error();
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-}
 
